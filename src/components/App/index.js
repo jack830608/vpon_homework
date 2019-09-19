2c047d40f86d101a39f51bbba7f2721c807e2339
jack <jack830608@gmail.com>
2019-09-19 17:41:25 +0800

    limit Ip address

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import './style.scss'
 import webSocket from 'socket.io-client'
+import axios from 'axios'
 
 export default () => {
+    const [myIp, setMyIp] = useState()
+    const [serverIp, setServerIp] = useState()
     const [ws, setWs] = useState(null)
     const [dir, setDir] = useState(false)
     useEffect(() => {
+        axios.get('https://api.ipify.org/').then((res) => {
+            setMyIp(res.data)
+        })
         setWs(webSocket())
     }, [])
 
@@ -18,35 +24,36 @@ export default () => {
 
     const initWebSocket = () => {
         ws.on('getMessageAll', (message) => {
-            setDir(message)
+            setDir(message.status)
+            setServerIp(message.ip)
         })
     }
 
-    const sendMessage = (status) => {
-        ws.emit('getMessageAll', status)
+    const sendMessage = (message) => {
+        ws.emit('getMessageAll', message)
 
     }
     window.onorientationchange = (event) => {
         if (window.orientation === 0) {
-            sendMessage(false)
+            sendMessage({ status: false, ip: myIp })
         } else {
-            sendMessage(true)
+            sendMessage({ status: true, ip: myIp })
         }
     };
 
     return (
-        <div className="container" style={{backgroundColor: dir ? 'rgba(255, 255, 255, 0)' : 'rgba(0, 0, 50, 0.5)'}}>
+        <div className="container" style={{ backgroundColor: dir && (serverIp === myIp) ? 'rgba(255, 255, 255, 0)' : 'rgba(0, 0, 50, 0.5)' }}>
             <img className="phone" src="/images/phone.png" />
-            {!dir &&<img className="rain" src="/images/rain.gif" />}
-            <div className="book p3d" style={{ transform: dir ? 'rotateX(32.5deg)' : 'rotateX(60deg)' }}>
+            {dir && (serverIp === myIp) ? '' : <img className="rain" src="/images/rain.gif" />}
+            <div className="book p3d" style={{ transform: dir && (serverIp === myIp) ? 'rotateX(32.5deg)' : 'rotateX(60deg)' }}>
                 <div className="back-cover p3d">
                     <div className="page back flip" />
                     <div className="page front p3d">
-                        <div className="shadow" style={{ transform: dir ? 'translateZ(1px) skewX(-22.5deg)' : 'translateZ(1px) skewX(0deg)', opacity: 0.4 }} />
-                        <div className="dino" style={{ transform: dir ? 'rotateX(-45deg)' : 'rotateX(0deg)' }} />
+                        <div className="shadow" style={{ transform: dir && (serverIp === myIp) ? 'translateZ(1px) skewX(-22.5deg)' : 'translateZ(1px) skewX(0deg)', opacity: 0.4 }} />
+                        <div className="dino" style={{ transform: dir && (serverIp === myIp) ? 'rotateX(-45deg)' : 'rotateX(0deg)' }} />
                     </div>
                 </div>
-                <div className="front-cover p3d" style={{ transform: dir ? 'rotateY(-180deg)' : 'rotateY(0deg)' }}>
+                <div className="front-cover p3d" style={{ transform: dir && (serverIp === myIp) ? 'rotateY(-180deg)' : 'rotateY(0deg)' }}>
                     <div className="page front flip p3d">
                         <p>非常感謝 Vpon 給我這個機會，在9/17面談完並了解工作內容以及公司內部氣氛後，非常希望能加入 Vpon 與公司及其他同事一起成長，期望在為公司解決問題之餘，同時成為組織內重要的成員。</p>
                         <p>歡迎隨時與我聯絡！</p>
